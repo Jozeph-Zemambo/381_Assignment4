b93d6834387597516af8052bca8be8c84abfc382
Aly Masani <alymohammed.masani@ucalgary.ca>
2024-03-24 18:32:03 -0600

    made some changes and added functionality to the products page

--- App/my-ecommerce-app/src/App.js
+++ App/my-ecommerce-app/src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/navbar';
 import AboutUs from './components/aboutus';
+import Productpage from './components/Productpage';
+
 // Placeholder components
 const HomePage = () => <h1>Home Page</h1>;
 const ProductsPage = () => <h1>Products Page</h1>;
@@ -15,7 +17,7 @@ function App() {
       <AboutUs />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/products" element={<Productpage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
