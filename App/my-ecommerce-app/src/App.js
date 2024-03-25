b154bdb5cd20ae6a9d76cd5fc5a1d8b9d12ae509
Jozeph-Zemamabo <joseph.kimambo@ucalgary.ca>
2024-03-25 15:20:50 -0600

    for now

--- App/my-ecommerce-app/src/App.js
+++ App/my-ecommerce-app/src/App.js
@@ -1,20 +1,16 @@
 // App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NavBar from './components/navbar';
-import AboutUs from './components/aboutus';
+
+import HomePage from './components/Homepage';
 import Productpage from './components/Productpage';
 
-// Placeholder components
-const HomePage = () => <h1>Home Page</h1>;
-const ProductsPage = () => <h1>Products Page</h1>;
-const LoginPage = () => <h1>Login Page</h1>;
+// Placeholder for the login page
+const LoginPage = () => <p>Login Page</p>;
 
 function App() {
   return (
     <Router>
-      <NavBar />
-      <AboutUs />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/products" element={<Productpage />} />
